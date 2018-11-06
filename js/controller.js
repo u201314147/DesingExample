c5079fcfbee01d2fffdeb231cf8973207be80058
u201314147 <u201314147@upc.edu.pe>
2018-11-06 12:33:36 -0500

    Pruebas node.js

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,4 @@
-    /*global BindClass */
+     /*global BindClass */
 /*jslint browser: true, indent: 4*/
 
 /**
@@ -40,22 +40,94 @@ function init()
 
 }
 
+function noti()
+{
+         $.ajax({
+          method: "GET",
+          url: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Flarepublica.pe%2Frss%2Fpolitica&api_key=w2a6pnhlxyj4t43vjerkemipaopxafz6ov7hegto",
+          crossDomain: true,
+          dataType: 'jsonp',
 
-      function appendPre(message) {
-   
-        var textContent = message + '\n';
-
-          info.set('asideBlock6Content1',textContent);
-           console.log(textContent);
+        })
+          .done(function( data ) {
+            console.log( "Data Saved:2"+data.status);
       
-      }
+      
+        var full = "";
+       var noticias = data.items;
+         
+       
+            for (var i = 0; i < noticias.length; i++) {
 
-function getCurrency(from, to, textdesign)
+
+              var noti = noticias[i];
+              var title = noti.title;
+                var foto = noti.enclosure.link;
+              var desc = noti.description;
+            
+
+            if(i ==0)
+            {  
+             full = full + '<div class="carousel-item active"> <img src="'+ foto +'"style="margin-top: -43px; height: 137px;" /> <label style = "color: white; font-family: "b-medium"; font-size: 20px; margin-left: 200px; display: inline-block; margin-top:-10px;"> ' + title +' </label> <label style = "color: white; font-family: "b-light-condensed"; font-size: 15px; margin-left: 200px; display: inline-block; margin-top:-10px;">'+ desc+' </label> </div>';
+            }
+            else
+            {
+            full = full + '<div class="carousel-item"> <img src="'+ foto +'"style="margin-top: -43px; height: 137px;" />  <label style = "color: white; font-family: "b-medium"; font-size: 20px; margin-left: 200px; display: inline-block; margin-top:-10px;"> ' + title +' </label> <label style = "color: white; font-family: "b-light-condensed"; font-size: 15px; margin-left: 200px; display: inline-block; margin-top:-10px;">'+ desc+' </label> </div>';
+          
+            }
+
+
+          }
+           console.log(foto);
+           console.log(full);
+          $("#333").html('<div id="carouselExampleControls" class="carousel slide" data-ride="carousel"> <div class="carousel-inner">'+ full + '</div> <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="true"></span"> <span class="sr-only">Previous</span> </a> <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next"> <span class="carousel-control-next-icon" aria-hidden="true"></span> <span class="sr-only">Next</span> </a> </div>'
+
+
+            )
+      }); 
+
+}
+ function darFormatoAldia(day,month,year) {
+    var formato ="";
+    switch(day) {
+        case "clear-day":
+           info.set('asideBlock11Content1',jsonData.climaSunny);
+        case "clear-night":
+           info.set('asideBlock11Content1',jsonData.climaCloudy);
+      case "rain":
+           info.set('asideBlock11Content1',jsonData.climaRain);
+        case "snow":
+           info.set('asideBlock11Content1',jsonData.climaSunny);
+
+      case "sleet":
+          info.set('asideBlock11Content1',jsonData.climaCloudy);
+        case "wind":
+         info.set('asideBlock11Content1',jsonData.climaCloudy);
+
+      case "fog":
+        info.set('asideBlock11Content1',jsonData.climaCloudy);
+
+     case "cloudy":
+           info.set('asideBlock11Content1',jsonData.climaCloudy);
+        case "partly-cloudy-day":
+           info.set('asideBlock11Content1',jsonData.climaCloudy);
+
+      case "partly-cloudy-night":
+           info.set('asideBlock11Content1',jsonData.climaCloudy);
+
+      default:
+           info.set('asideBlock11Content1',jsonData.climaSunny);
+    }
+   } 
+
+
+
+function getCurrency(from, to, textdesign, urls)
 {
     $.ajax({
           method: "GET",
          //url: "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency="+from+"&to_currency="+to+"&apikey=2AS9DR6NX2GY2SW5",
-          url: 'https://marketdata.websol.barchart.com/getQuote.json?apikey=f40b136c6dc4451f9136bb53b9e70ffa&symbols=ZC*1,IBM,GOOGL,ADES,EEUU,ADES,ASIX,AEGN,AMTX,APD,AKS,AIN,ALB,ATI,AMRK,AMRC,AVD,AMWD,AMRS,AQMS,RKDA,AGX,ATIS,ATISW,AXTA,%5EEURUSD',
+          url: urls,
          
           crossDomain: true,
           dataType: 'json',
@@ -73,8 +145,8 @@ function getCurrency(from, to, textdesign)
                   var open1 = data.results[i].open
 
                   var close1 = data.results[i].close
-                  var result = (Math.round(close1*100 -  open1*100))/100  
-              
+                  var result = ((Math.round(close1*100 -  open1*100))/100).toFixed(2)
+                  
                   if(result> 0)
                   {
                     result =  '<p style="color: green; display:inline;">↑</p>'+ result
@@ -89,12 +161,12 @@ function getCurrency(from, to, textdesign)
                   }
                  //  console.log(result)
 
-                   full = full + data.results[i].symbol + result + " "
+                   full = full  + " " + data.results[i].symbol + result + " "
 
 
               }
-              $("#111").html('<marquee scrollamount="2" direction="right" scrolldelay="70" behavior="scroll">'+full+'</marquee>');
-              $("#222").html('<marquee scrollamount="2" direction="right" scrolldelay="70" behavior="scroll">'+full+'</marquee>');
+              $(textdesign).html('<marquee direction="left" scrolldelay="100" behavior="scroll">'+full+'</marquee>');
+              $(textdesign).html('<marquee direction="left" scrolldelay="100" behavior="scroll">'+full+'</marquee>');
          //   info.set(textdesign, full);
               console.log(full)
 
@@ -120,125 +192,29 @@ function getCurrency(from, to, textdesign)
             var value = obj["Realtime Currency Exchange Rate"]["5. Exchange Rate"];
 
             info.set(textdesign, from + " " + value);*/
-}
-
-function noti()
-{
-         $.ajax({
-          method: "GET",
-          url: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Felcomercio.pe%2Ffeed%2Fgastronomia",
-          crossDomain: true,
-          dataType: 'jsonp',
-
-        })
-          .done(function( data ) {
-            console.log( "Data Saved: "+data.status);
-      
-
-       var noticias = data.items;
-         
-         if (noticias.length > 0) {
-            for (var i = 0; i < noticias.length; i++) {
-              var noti = noticias[i];
-              var desc = noti.title;
-              if (!desc) {
-                desc = noti.title;
-              }
-
-                if(i==0){   info.set('asideBlock7Content2',desc); }
-                if(i==1){   info.set('asideBlock7Content3',desc); }
-                if(i==2){   info.set('asideBlock8Content1',desc); }
-                if(i==3){   info.set('asideBlock8Content2',desc); }
-
-
-            }
-          } else {
-            appendPre('No hay noticias');
-          }
-        
-
-      }); 
-
-}
- 
+}  
 function updateTime()
 {
       var dt = new Date();
-      var time = null;
-
-
-      if(dt.getMinutes() <10)
-      { 
-
-         time = dt.getHours() + ":0" + dt.getMinutes() + " AM";
-
-
-    //    if(dt.getSeconds() <10)
-    //   time = dt.getHours() + ":0" + dt.getMinutes() +":0" +dt.getSeconds()
-    //  else
-   //      time = dt.getHours() + ":0" + dt.getMinutes() +":" +dt.getSeconds()
-    if(dt.getHours() == 0)
-        {
-             time = dt.getHours()+12 + ":0" + dt.getMinutes() + " AM";
-        }
-
-         if(dt.getHours() == 12)
-        {
-             time = dt.getHours() + ":0" + dt.getMinutes() + " PM";
-        }
-        if(dt.getHours() > 12)
-        {
-             time = dt.getHours()-12 + ":0" + dt.getMinutes() + " PM";
-        }
-       }   
-    else
-      {
-
-          time = dt.getHours() + ":" + dt.getMinutes() + " AM";
-
-
-         //if(dt.getSeconds() <10)
-    //   time = dt.getHours() + ":" + dt.getMinutes() +":0" +dt.getSeconds()
-    //  else
-    //     time = dt.getHours() + ":" + dt.getMinutes() +":" +dt.getSeconds()
-     if(dt.getHours() == 0)
-        {
-             time = dt.getHours()+12 + ":" + dt.getMinutes() + " AM";
-        }
-     if(dt.getHours() == 12)
-        {
-             time = dt.getHours() + ":" + dt.getMinutes() + " PM";
-        }
-       if(dt.getHours() > 12)
-        {
-             time = dt.getHours()-12 + ":" + dt.getMinutes() + " PM";
-        }
-       } 
+      var time = dt.toLocaleTimeString('en-US', {hour: '2-digit', minute: '2-digit', hour12: true});    
        info.set('asideBlock1Content1', time);
-
 }
     function bindData(jsonData) {
         info = new BindClass('templateSix');
         console.log("ssds");
 
 updateTime();
-//WEA
-
 noti();
-//WEA
-
-getCurrency("USD","PEN", "asideBlock15Content3");
-getCurrency("EUR","PEN", "asideBlock16Content2");
-
 var meses = [
-  "Enero", "Febrero", "Marzo",
-  "Abril", "Mayo", "Junio", "Julio",
-  "Agosto", "Septiembre", "Octubre",
-  "Noviembre", "Diciembre"
+  "enero", "febrero", "marzo",
+  "abril", "mayo", "junio", "julio",
+  "agosto", "septiembre", "octubre",
+  "noviembre", "diciembre"
 ]
    var dias=["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sábado"];
 
 var date = new Date();
+
 var dia = date.getDate();
 var mes = date.getMonth();
 var yyy = date.getFullYear();
@@ -256,11 +232,6 @@ info.set('asideBlock1Content3', dateDay);
   //  info.set('asideBlock1Content3', jsonData.asideBlock1Content3);
 
  window.setInterval(updateTime,100);
-
-
-
-
-
         $.ajax({
           method: "GET",
           url: "https://api.darksky.net/forecast/7af51a01e29c8ddb7a548fad3cf35a05/-12.193731,-76.708493?units=si",
@@ -276,46 +247,45 @@ info.set('asideBlock1Content3', dateDay);
                  info.set('asideBlock11Content2', "↑"+ changeicon2(data.daily.data[0].icon, Math.round(data.daily.data[0].temperatureMax )) );
              info.set('asideBlock11Content3',"↓"+ changeicon2(data.daily.data[0].icon, Math.round(data.daily.data[0].temperatureMin )));
        
-              function changeiconTitle(icon, temp) {
+function changeiconTitle(icon, temp) {
     switch(icon) {
-       case "clear-day":
-           info.set('asideBlock11Content1',jsonData.clima_clear);
+        case "clear-day":
+           return info.set('asideBlock11Content1',jsonData.weather_clear);
         case "clear-night":
-           info.set('asideBlock11Content1',jsonData.clima_clearnight);
+           return info.set('asideBlock11Content1',jsonData.weather_clearnight);
         case "cloudy":
-           info.set('asideBlock11Content1',jsonData.clima_cloudy);
+           return info.set('asideBlock11Content1',jsonData.weather_cloudy);
         case "fog":
-        info.set('asideBlock11Content1',jsonData.clima_fog);
+           return info.set('asideBlock11Content1',jsonData.weather_fog);
         case "partly-cloudy-day":
-           info.set('asideBlock11Content1',jsonData.clima_partlycloudy);
+           return info.set('asideBlock11Content1',jsonData.weather_partlycloudy);
         case "partly-cloudy-night":
-           info.set('asideBlock11Content1',jsonData.clima_partlycloudynight);
+           return info.set('asideBlock11Content1',jsonData.weather_partlycloudynight);
         case "rain":
-           info.set('asideBlock11Content1',jsonData.clima_rain);
+           return info.set('asideBlock11Content1',jsonData.weather_rain);
         case "sleet":
-          info.set('asideBlock11Content1',jsonData.clima_sleet);
+           return info.set('asideBlock11Content1',jsonData.weather_sleet);
         case "snow":
-           info.set('asideBlock11Content1',jsonData.clima_snow);
+           return info.set('asideBlock11Content1',jsonData.weather_snow);
+        case "sunny":
+           return info.set('asideBlock11Content1',jsonData.weather_sunny);
         case "wind":
-         info.set('asideBlock11Content1',jsonData.clima_wind);
-      default:
-           info.set('asideBlock11Content1',jsonData.clima_sunny);
+           return info.set('asideBlock11Content1',jsonData.weather_sleet);
     }
    } 
 
 
 
             
-             function changeicon(icon, temp) {
-            return temp+"°";
-
-            
+   function changeicon(icon, temp) {
+      return temp+"°";           
    } 
 
-        function changeicon2(icon, temp) {
-            return temp +"°C";
-     }
-             function getDayString(number) {
+   function changeicon2(icon, temp) {
+      return temp +"°C";
+   }
+   
+   function getDayString(number) {
     switch(number) {
         case 0:
             return "D";
@@ -360,18 +330,60 @@ for(var i=0; i<7; ++i) {
 }
 
 
+getCurrency("USD","PEN", "#111",'https://marketdata.websol.barchart.com/getQuote.json?apikey=f40b136c6dc4451f9136bb53b9e70ffa&symbols=ZC*1,IBM,GOOGL,ADES,EEUU,ADES,ASIX,AEGN,AMTX,APD,AKS,AIN,ALB,ATI,AMRK,AMRC,AVD,AMWD,AMRS,AQMS,RKDA,AGX,ATIS,ATISW,AXTA,%5EEURUSD');
+getCurrency("EUR","PEN", "#222",'https://marketdata.websol.barchart.com/getQuote.json?apikey=f40b136c6dc4451f9136bb53b9e70ffa&symbols=ZC*1,ACH,APWC,BHP,BAK,EVGN,MT,CSTM,GOLD,TS,LYB,TX,TS,UN,UL,RIO,PKX,SHI,TANH,NEWA,GURE,%5EEURUSD');
+
 $("#tablaClima").html(html);
 
 changeiconTitle(data.currently.icon, Math.round(data.currently.temperature));
 
 var html2 = changeicon2(data.currently.icon, Math.round(data.currently.temperature));
+
+
+function levenshtein(s1, s2) {
+  var l1 = s1.length;
+  var l2 = s2.length;
+  var d = [];
+  var c = 0;
+  var a = 0;
+
+  if(l1 == 0)
+    return l2;
+
+  if(l2 == 0)
+    return l1;
+
+  var d = new Array((l1 + 1) * (l2 + 1));
+  a = l1 + 1;
+
+  for(var i = 0; i <= l1; d[i] = i++);
+  for(var j = 0; j <= l2; d[j * a] = j++);
+
+  for(var i = 1; i <= l1; i++) {
+    for(var j = 1; j <= l2; j++) {
+      if(s1[i - 1] == s2[j - 1])
+        c = 0;
+      else
+        c = 1;
+      var r = d[j * a + i - 1] + 1;
+      var s = d[(j - 1) * a + i] + 1;
+      var t = d[(j - 1) * a + i - 1] + c;
+
+      d[j * a + i] = Math.min(Math.min(r, s), t);
+    }
+  }
+
+  return(d[l2 * a + l1]);
+}
+ ///indica la cantidad de cambios de caracterees para que sean iguales  == 5
+console.log(levenshtein("Lonche con mamá","El Lonche con mi mamá"));
+
 $("#logoClima").html(html2);
 
           });
 
         
         info.set('borderClima',jsonData.borderClima);
-         info.set('logoHilton',jsonData.hiltonlogo);
         /* Header Div */
         info.set('headerImage', jsonData.headerImage);
         /* Showcase Div */
@@ -382,6 +394,7 @@ $("#logoClima").html(html2);
         info.set('showcaseContent', jsonData.showcaseContent);
         /* Image Text Overlay */
         info.set('bannerImg', jsonData.bannerImg);
+        info.set('NowNewsImage', jsonData.NowNewsImage);
        
         info.set('NowPlayingImage', jsonData.NowPlayingImage);
         info.set('bannerImgOverlayText', jsonData.bannerImgOverlayText);
@@ -397,40 +410,24 @@ $("#logoClima").html(html2);
         info.set('asideBlock1Content2', jsonData.asideBlock1Content2);
 
         // Block 2
+        info.set('notificationtext', jsonData.notificationtext);
         info.set('notification0text', jsonData.notification0text);
         info.set('notification1text', jsonData.notification1text);
         info.set('notification2text', jsonData.notification2text);
         info.set('notification3text', jsonData.notification3text);
-         info.set('notification0Xtext', jsonData.notification0textX);
-        info.set('notification1Xtext', jsonData.notification1textX);
-        info.set('notification2Xtext', jsonData.notification2textX);
-        info.set('notification3Xtext', jsonData.notification3textX);
-         info.set('notification0Ytext', jsonData.notification0textY);
-        info.set('notification1Ytext', jsonData.notification1textY);
-        info.set('notification2Ytext', jsonData.notification2textY);
-        info.set('notification3Ytext', jsonData.notification3textY);
-    
+        info.set('notification4text', jsonData.notification4text);
 
         info.set('asideBlock3Content2', jsonData.asideBlock3Content2);
         info.set('asideBlock3Content3', jsonData.asideBlock3Content3);
         // Block 4
-        info.set('asideBlock4Content1',jsonData.asideBlock4Content1);
+        info.set('asideBlock4Content1', jsonData.asideBlock4Content1);
         info.set('asideBlock4Content2', jsonData.asideBlock4Content2);
 		info.set('asideBlock4Content3', jsonData.asideBlock4Content3);
         // Block 5
         info.set('asideBlock5Content1', jsonData.asideBlock5Content1);
         info.set('asideBlock5Content2', jsonData.asideBlock5Content2);
         info.set('asideBlock5Content3', jsonData.asideBlock5Content3);
-
-        info.set('asideBlock4Content3X', jsonData.asideBlock4Content3X);
-        // Block 5
-        info.set('asideBlock5Content1X', jsonData.asideBlock5Content1X);
-        info.set('asideBlock5Content2X', jsonData.asideBlock5Content2X);
-        info.set('asideBlock5Content3X', jsonData.asideBlock5Content3X);
-
-        
         // Block 6
-        info.set('asideBlock6Content0', jsonData.asideBlock6Content0);
         info.set('asideBlock6Content1', jsonData.asideBlock6Content1);
         info.set('asideBlock6Content2', jsonData.asideBlock6Content2);
         info.set('asideBlock6Content3', jsonData.asideBlock6Content3);
@@ -461,22 +458,22 @@ $("#logoClima").html(html2);
         info.set('asideBlock14Content2', jsonData.asideBlock14Content2);
         info.set('asideBlock14Content3', jsonData.asideBlock14Content3);
 
-          info.set('asideBlock13Content2X', jsonData.asideBlock13Content2X + "    .");
-        info.set('asideBlock13Content3X', jsonData.asideBlock13Content3X + "    .");
+          info.set('asideBlock13Content2X', jsonData.asideBlock13Content2X);
+        info.set('asideBlock13Content3X', jsonData.asideBlock13Content3X);
         // Block 4
-        info.set('asideBlock14Content1X', jsonData.asideBlock14Content1X + "    . ");
-        info.set('asideBlock14Content2X', jsonData.asideBlock14Content2X + "        .");
-        info.set('asideBlock14Content3X', jsonData.asideBlock14Content3X + "    .");
+        info.set('asideBlock14Content1X', jsonData.asideBlock14Content1X);
+        info.set('asideBlock14Content2X', jsonData.asideBlock14Content2X);
+        info.set('asideBlock14Content3X', jsonData.asideBlock14Content3X);
 
 
 
         // Block 5
         info.set('asideBlock15Content1', jsonData.asideBlock15Content1);
         info.set('asideBlock15Content2', jsonData.asideBlock15Content2);
-       // info.set('asideBlock15Content3', jsonData.asideBlock15Content3);  CURRENCY
+        info.set('asideBlock15Content3', jsonData.asideBlock15Content3);
         // Block 6
         info.set('asideBlock16Content1', jsonData.asideBlock16Content1);
-      //  info.set('asideBlock16Content2', jsonData.asideBlock16Content2);   CURRENCY2
+        info.set('asideBlock16Content2', jsonData.asideBlock16Content2);
         info.set('asideBlock16Content3', jsonData.asideBlock16Content3);
         // Block 7
         info.set('asideBlock17Content1', jsonData.asideBlock17Content1);
